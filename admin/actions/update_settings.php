ad13fc1d3710120de1034597ea053daaaccbe7aa
Norman Palisoc <norman.consultant@platform-11.com>
2024-05-01 21:20:24 +0800

    feat: update other pages

---
+++ admin/actions/update_settings.php
@@ -0,0 +1,81 @@
+<?php
+  include('../../utils/connections.php');
+  session_start();
+  try {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      if (
+        isset($_POST['user_id']) &&
+        isset($_POST['user_email']) &&
+        isset($_POST['first_name']) &&
+        isset($_POST['last_name']) &&
+        isset($_POST['email']) &&
+        isset($_POST['phone']) &&
+        isset($_POST['address']) &&
+        isset($_POST['gender']) &&
+        isset($_POST['birth_date'])
+      ) {
+        $user_id = $conn->real_escape_string($_POST['user_id']);
+        $user_email = $conn->real_escape_string($_POST['user_email']);
+        $first_name = $conn->real_escape_string($_POST['first_name']);
+        $last_name = $conn->real_escape_string($_POST['last_name']);
+        $email = $conn->real_escape_string($_POST['email']);
+        $phone = $conn->real_escape_string($_POST['phone']);
+        $address = $conn->real_escape_string($_POST['address']);
+        $gender = $conn->real_escape_string($_POST['gender']);
+        $birth_date = $conn->real_escape_string($_POST['birth_date']);
+        $update_query = "
+          UPDATE user_info SET 
+          first_name = ?,
+          last_name = ?,
+          phone = ?,
+          address = ?,
+          gender = ?,
+          birth_date = ? 
+          WHERE id = ? AND email = ?";
+        $stmt = $conn->prepare($update_query);
+        $stmt->bind_param(
+          'ssssssss',
+          $first_name,
+          $last_name,
+          $phone,
+          $address,
+          $gender,
+          $birth_date,
+          $user_id,
+          $user_email
+        );
+        $result = $stmt->execute();
+        if ($result == 1) {
+          $_SESSION['user_info.first_name'] = $first_name;
+          $_SESSION['user_info.last_name'] = $last_name;
+          $_SESSION['user_info.phone'] = $phone;
+          $_SESSION['user_info.address'] = $address;
+          $_SESSION['user_info.gender'] = $gender;
+          $_SESSION['user_info.birthdate'] = $birthdate;
+          unset($_SESSION['errors.type']);
+          unset($_SESSION['errors.title']);
+          unset($_SESSION['errors.message']);
+          $stmt->close();
+          $conn->close();
+        } else {
+          $_SESSION['errors.type'] = 'update_settings_error';
+          $_SESSION['errors.title'] = 'Something went wrong';
+          $_SESSION['errors.message'] = 'Unable to update settings, please check user info if email already exists.';
+        }
+        header('Location: ../settings/');
+      } else {
+        $_SESSION['errors.type'] = 'update_settings_error';
+        $_SESSION['errors.title'] = 'Something went wrong';
+        $_SESSION['errors.message'] = 'Unable to update settings, might be missing some required parameters';
+        header('Location: ../settings/');
+      }
+    } else {
+      $_SESSION['errors.type'] = 'update_settings_error';
+      $_SESSION['errors.title'] = 'Something went wrong';
+      $_SESSION['errors.message'] = 'Unable to update settings, might be missing some required parameters';
+      header('Location: ../settings/');
+    }
+  } catch (\Throwable $th) {
+    echo $th;
+  }
+?>
--- admin/settings/index.php
+++ admin/settings/index.php
@@ -1,6 +1,6 @@
 <?php
   session_start();
-  include('../utils/connections.php');
+  include('../../utils/connections.php');
   if (
     !isset($_SESSION['user_credentials.username']) &&
     !isset($_SESSION['user_credentials.type'])
@@ -11,6 +11,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <title>Admin / Product Variants</title>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
@@ -18,6 +19,7 @@
       integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
       crossorigin="anonymous"
     >
+    <link href="https://cdn.datatables.net/2.0.5/css/dataTables.bootstrap5.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
@@ -30,7 +32,7 @@
         <h4 class="h4-tm-title sans-700 color-dark-grey background-color-yellow">ICYLICIOUS&trade;</h4>
       </div>
       <div class="div-sidebar-logo">
-        <i class="fa-solid fa-circle-user color-white" style="font-size: 40pt; margin-top: -5px"></i>
+        <i class="fa-solid fa-circle-user color-white" style="font-size: 30pt; margin-top: -8px"></i>
         <div>
           <h4 class="color-white sans-700 h6-title-name">
             <?php
@@ -47,7 +49,7 @@
       <div class="div-siderbar-menu-links">
         <button
           id="btn-dashboard"
-          class="btn btn-outline-success 
+          class="btn btn-outline-success btn-sm
             sans-regular 
             background-color-super-light-grey 
             border-color-super-light-grey 
@@ -59,7 +61,7 @@
         </button>
         <button
           id="btn-user-management"
-          class="btn btn-outline-success 
+          class="btn btn-outline-success btn-sm
             sans-regular 
             background-color-super-light-grey 
             border-color-super-light-grey 
@@ -71,7 +73,7 @@
         </button>
         <button 
           id="btn-categories"
-          class="btn btn-outline-success 
+          class="btn btn-outline-success btn-sm
             sans-regular 
             background-color-super-light-grey 
             border-color-super-light-grey 
@@ -83,7 +85,7 @@
         </button>
         <button 
           id="btn-products" 
-          class="btn btn-outline-success 
+          class="btn btn-outline-success btn-sm
             sans-regular 
             background-color-super-light-grey 
             border-color-super-light-grey 
@@ -95,7 +97,7 @@
         </button>
         <button
           id="btn-variants"
-          class="btn btn-outline-success 
+          class="btn btn-outline-success btn-sm
             sans-regular 
             background-color-super-light-grey 
             border-color-super-light-grey 
@@ -107,7 +109,7 @@
         </button>
         <button 
           id="btn-orders" 
-          class="btn btn-outline-success 
+          class="btn btn-outline-success btn-sm
             sans-regular 
             background-color-super-light-grey 
             border-color-super-light-grey 
@@ -119,7 +121,7 @@
         </button>
         <button 
           id="btn-settings" 
-          class="btn btn-outline-success 
+          class="btn btn-outline-success btn-sm
             sans-700 
             background-color-yellow 
             border-color-yellow 
@@ -130,40 +132,129 @@
           <i class="fa-solid fa-gear"></i><span style="padding-left: 19px">Settings</span>
         </button>
       </div>
+      <div class="div-siderbar-profile-links">
+        <button
+          class="btn btn-outline-success btn-sm
+            sans-regular 
+            color-white 
+            btn-menu 
+            btn-menu-unselected
+            btn-logout"
+          style="border: 0px"
+          type="button">
+          <i class="fa-solid fa-right-from-bracket"></i><span style="padding-left: 19px">Logout</span>
+        </button>
+      </div>
     </div>
     <div class="content">
-      <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary navbar-fixed-top">
+      <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
         <div class="container-fluid">
-          <a class="navbar-brand sans-regular color-white a-navbar-path" href="#" style="cursor: default;">
-            &nbsp;&nbsp;<i class="fa-solid fa-bars" style="cursor: pointer"></i>
-            &nbsp;&nbsp;&nbsp;<b>Admin</b>&nbsp;
+          <a class="navbar-brand sans-regular color-dark-grey a-navbar-path" href="#" style="cursor: default;">
+            &nbsp;&nbsp;&nbsp;<i class="fa-solid fa-bars" style="cursor: pointer"></i>
+            &nbsp;&nbsp;&nbsp;&nbsp;<b>Admin</b>&nbsp;
             <i class="fa-solid fa-chevron-right"></i>
             &nbsp;Settings
           </a>
-          <div>
-            <button 
-              class="btn btn-success 
-                sans-regular 
-                background-color-yellow 
-                border-color-yellow 
-                color-dark-grey 
-                btn-profile"
-              type="button">
-              <i class="fa-solid fa-circle-user"></i>&nbsp;&nbsp;Profile
-            </button>
-            <button
-              class="btn btn-outline-success 
-                sans-regular 
-                border-color-yellow 
-                color-yellow 
-                btn-logout"
-              type="button">
-              <i class="fa-solid fa-right-from-bracket"></i>
-            </button>
-          </div>
         </div>
       </nav>
-      <div class="container">
+      <div>
+        <div class="content-wrapper">
+          <div class="div-content-title">
+            <div class="div-content-title-labels">
+              <h4 class="color-dark-grey sans-600" style="font-size: 13pt;">
+                Manage Profile Settings
+              </h4>
+              <p class="color-super-light-grey sans-regular" style="font-size: 10pt;">
+                Update administrator-related information
+              </p>
+            </div>
+          </div>
+          <div style="margin-top: 20px;">
+            <form action="../actions/update_settings.php" method="POST">
+              <input type="hidden" value="<?php echo $_SESSION['user_info.user_id'] ?>" name="user_id" />
+              <input type="hidden" value="<?php echo $_SESSION['user_info.email'] ?>" name="user_email" />
+              <div class="row">
+                <div class="col-lg-7 col-md-7 col-sm-12">
+                  <div style="display: flex; gap: 10px">
+                    <div style="flex: 1">
+                      <input 
+                        id="edit-fn" 
+                        type="text" 
+                        placeholder="First Name" 
+                        name="first_name" 
+                        required 
+                        class="sans-regular" 
+                        value="<?php echo $_SESSION['user_info.first_name'] ?>"
+                      />
+                    </div>
+                    <div style="flex: 1">
+                      <input 
+                        id="edit-ln" 
+                        type="text" 
+                        placeholder="Last Name" 
+                        name="last_name" 
+                        required 
+                        class="sans-regular"
+                        value="<?php echo $_SESSION['user_info.last_name'] ?>"
+                      />
+                    </div>
+                  </div>
+                  <input 
+                    id="edit-ea" 
+                    type="email" 
+                    placeholder="Email Address (eg. myemail@gmail.com)" 
+                    name="email" 
+                    required 
+                    readonly 
+                    class="sans-regular is_readonly"
+                    value="<?php echo $_SESSION['user_info.email'] ?>"
+                  />
+                  <input 
+                    id="edit-pn" 
+                    type="text" 
+                    placeholder="Mobile No. (eg. +639__)" 
+                    name="phone" 
+                    required 
+                    class="sans-regular"
+                    value="<?php echo $_SESSION['user_info.phone'] ?>"
+                  />
+                  <input 
+                    id="edit-ad" 
+                    type="text" 
+                    placeholder="Address (eg. Ayala Makati, Metro Manila, Philippines)" 
+                    name="address" 
+                    required 
+                    class="sans-regular"
+                    value="<?php echo $_SESSION['user_info.address'] ?>"
+                  />
+                  <div style="display: flex; gap: 10px; margin-top: 8px">
+                    <div style="flex: 1; display: flex; flex-direction: column">
+                      <label for="birth_date" class="sans-600">Select Gender</label>
+                      <select id="edit-gd" class="form-select sans-regular gender-select" name="gender" style="flex: 1">
+                        <option value="Male" <?php if ($_SESSION['user_info.gender'] == "Male") echo 'selected="selected"' ?>>Male</option>
+                        <option value="Female" <?php if ($_SESSION['user_info.gender'] == "Female") echo 'selected="selected"' ?>>Female</option>
+                        <option value="Others" <?php if ($_SESSION['user_info.gender'] == "Others") echo 'selected="selected"' ?>>Others</option>
+                      </select>
+                    </div>
+                    <div style="flex: 1">
+                      <label for="birth_date" class="sans-600">Birth Date</label>
+                      <input 
+                        id="edit-bd" 
+                        type="date" 
+                        placeholder="Birth Date" 
+                        name="birth_date" 
+                        required 
+                        class="sans-regular"
+                        value="<?php echo $_SESSION['user_info.birthdate'] ?>"
+                      />
+                    </div>
+                  </div>
+                  <button type="submit" class="btn btn-primary sans-600" style="margin-top: 20px;">Update Account</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   </body>
@@ -174,6 +265,8 @@
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
     crossorigin="anonymous">
   </script>
+  <script src="https://cdn.datatables.net/2.0.5/js/dataTables.js"></script>
+  <script src="https://cdn.datatables.net/2.0.5/js/dataTables.bootstrap5.js"></script>
   <script type="text/javascript">
     $(document).ready(() => {
       $('#btn-dashboard').click(() => {
@@ -200,6 +293,10 @@
       $('.btn-logout').click(() => {
         window.location.href = "../actions/logout.php";
       });
+      $('#data').dataTable({
+        'bLengthChange': false,
+        'searching': false
+      });
     });
-  </script>
+  </script>   
 </html>
