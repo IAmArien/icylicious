5d10a93936ad5a2602ea509fc8cf2a39296e6025
Norman Palisoc <norman.consultant@platform-11.com>
2024-05-05 14:56:38 +0800

    feat: initial structure of home

--- index.php
+++ index.php
@@ -16,25 +16,25 @@
     <link rel="stylesheet" href="./assets/css/index.css" />
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-fixed bg-dark">
+    <nav class="navbar navbar-expand-lg fixed-top bg-dark">
       <div class="container">
-        <a class="navbar-brand sans-600 color-white" href="#">ICYLICIOUS&trade;</a>
+        <a class="navbar-brand sans-600 color-white" href="./">ICYLICIOUS&trade;</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <i class="fa-solid fa-bars color-white"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link sans-600 active" aria-current="page" href="./">Home</a>
+              <a class="nav-link sans-600 size-11 active" aria-current="page" href="./">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link sans-regular" aria-current="page" href="./customer/menu/">Menu</a>
+              <a class="nav-link sans-regular size-11" aria-current="page" href="./customer/menu/">Menu</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link sans-regular" aria-current="page" href="./customer/about/">About Us</a>
+              <a class="nav-link sans-regular size-11" aria-current="page" href="./customer/about/">Reviews</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link sans-regular" aria-current="page" href="./customer/contact/">Contact Us</a>
+              <a class="nav-link sans-regular size-11" aria-current="page" href="./customer/contact/">Contact Us</a>
             </li>
           </ul>
           <form class="d-flex">
@@ -44,7 +44,174 @@
         </div>
       </div>
     </nav>
-    <div class="container">
+    <div class="content-wrapper">
+      <div>
+        <img src="./assets/images/home_background.png" class="img-background" />
+        <div class="background-overlay">
+          <h2 id="sub-description" class="color-white sans-italic-600">" Satisfying your palates since 2019. "</h2>
+          <p class="color-white sans-italic">17 stores nationwide and counting!</p>
+          <button class="btn btn-lg btn-primary sans-600" type="button">Order Now!</button>
+        </div>
+      </div>
+      <div class="container" style="padding-block: 50px;">
+        <div id="menu">
+          <h2 class="color-dark-grey sans-bold" style="text-align: center;">Best Sellers</h2>
+          <p class="color-light-grey sans-regular" style="text-align: center;">
+            Experience the ultimate taste of summer with our 5.5 summer promo
+          </p>
+          <div class="row" style="margin-top: 60px;">
+            <div class="col-lg-3 col-md-4 col-sm-12 div-product-info">
+              <img src="./assets/images/summer_promo_5_5_1.png" class="img-product" />
+              <div style="height: 20px;"></div>
+              <h4 class="color-dark-grey size-13 sans-700">
+                BUY 1 TAKE 1 - LARGE NUTTY-OREO NUTELLA SMOOTHIE WITH PEARLS
+              </h4>
+              <p class="color-light-grey size-10 sans-regular">
+                Experience the ultimate taste of summer with our 5.5 Summer Promo!
+                Available at all Icylicious Stores.
+              </p>
+              <div class="div-price-container">
+                <h3 class="color-dark-grey sans-bold">₱255.00</h3>
+                <h5 class="strike-price color-super-light-grey sans-regular">₱270.00</h5>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-12 div-product-info">
+              <img src="./assets/images/summer_promo_5_5_2.png" class="img-product" />
+              <div style="height: 20px;"></div>
+              <h4 class="color-dark-grey size-13 sans-700">
+                1 LARGE BUBBLE MILK TEA
+              </h4>
+              <p class="color-light-grey size-10 sans-regular">
+                Experience the ultimate taste of summer with our 5.5 Summer Promo!
+                Available at all Icylicious Stores.
+              </p>
+              <div class="div-price-container">
+                <h3 class="color-dark-grey sans-bold">₱55.00</h3>
+                <h5 class="strike-price color-super-light-grey sans-regular">₱70.00</h5>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-12 div-product-info">
+              <img src="./assets/images/summer_promo_5_5_3.png" class="img-product" />
+              <div style="height: 20px;"></div>
+              <h4 class="color-dark-grey size-13 sans-700">
+                P55 EACH (ANY FLAVOR OF YOUR CHOICE WHEN YOU BUY 2 REGULAR MILK TEAS)
+              </h4>
+              <p class="color-light-grey size-10 sans-regular">
+                Experience the ultimate taste of summer with our 5.5 Summer Promo!
+                Available at all Icylicious Stores.
+              </p>
+              <div class="div-price-container">
+                <h3 class="color-dark-grey sans-bold">₱55.00</h3>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-12 div-product-info">
+              <img src="./assets/images/summer_promo_5_5_4.png" class="img-product" />
+              <div style="height: 20px;"></div>
+              <h4 class="color-dark-grey size-13 sans-700">
+                BUY 1 TAKE 1 REGULAR CHOCO CHIP SMOOTHIE PEARLS
+              </h4>
+              <p class="color-light-grey size-10 sans-regular">
+                Experience the ultimate taste of summer with our 5.5 Summer Promo!
+                Available at all Icylicious Stores.
+              </p>
+              <div class="div-price-container">
+                <h3 class="color-dark-grey sans-bold">₱155.00</h3>
+                <h5 class="strike-price color-super-light-grey sans-regular">₱170.00</h5>
+              </div>
+              <!-- <button class="btn btn-md btn-outline-primary sans-600 btn-add-to-cart" type="button" style="width: 100%;">
+                <i class="fa-solid fa-cart-plus"></i>&nbsp;&nbsp;Add to cart
+              </button> -->
+            </div>
+          </div>
+          <div class="row" style="margin-top: 80px;">
+            <div class="col-lg-4 col-md-6 col-sm-12">
+              <img src="./assets/images/best_deals_1.png" class="img-product-deals" />
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12">
+              <img src="./assets/images/best_deals_2.png" class="img-product-deals" />
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12">
+              <img src="./assets/images/best_deals_3.png" class="img-product-deals" />
+            </div>
+          </div>
+        </div>
+        <div id="our-products" style="margin-top: 70px;">
+          <h2 class="color-dark-grey sans-bold" style="text-align: center;">Our Products</h2>
+          <p class="color-light-grey sans-regular" style="text-align: center;">
+            Don't miss out! Grab yours today at your fave Icylicious Store.
+          </p>
+          <div class="row" style="margin-top: 60px;">
+            <div class="col-lg-3 col-md-4 col-sm-12 div-product-info">
+              <img src="./assets/images/summer_promo_5_5_1.png" class="img-product" />
+              <div style="height: 20px;"></div>
+              <h4 class="color-dark-grey size-13 sans-700">
+                BUY 1 TAKE 1 - LARGE NUTTY-OREO NUTELLA SMOOTHIE WITH PEARLS
+              </h4>
+              <p class="color-light-grey size-10 sans-regular">
+                Experience the ultimate taste of summer with our 5.5 Summer Promo!
+                Available at all Icylicious Stores.
+              </p>
+              <div class="div-price-container">
+                <h3 class="color-dark-grey sans-bold">₱255.00</h3>
+                <h5 class="strike-price color-super-light-grey sans-regular">₱270.00</h5>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-12 div-product-info">
+              <img src="./assets/images/summer_promo_5_5_2.png" class="img-product" />
+              <div style="height: 20px;"></div>
+              <h4 class="color-dark-grey size-13 sans-700">
+                1 LARGE BUBBLE MILK TEA
+              </h4>
+              <p class="color-light-grey size-10 sans-regular">
+                Experience the ultimate taste of summer with our 5.5 Summer Promo!
+                Available at all Icylicious Stores.
+              </p>
+              <div class="div-price-container">
+                <h3 class="color-dark-grey sans-bold">₱55.00</h3>
+                <h5 class="strike-price color-super-light-grey sans-regular">₱70.00</h5>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-12 div-product-info">
+              <img src="./assets/images/summer_promo_5_5_3.png" class="img-product" />
+              <div style="height: 20px;"></div>
+              <h4 class="color-dark-grey size-13 sans-700">
+                P55 EACH (ANY FLAVOR OF YOUR CHOICE WHEN YOU BUY 2 REGULAR MILK TEAS)
+              </h4>
+              <p class="color-light-grey size-10 sans-regular">
+                Experience the ultimate taste of summer with our 5.5 Summer Promo!
+                Available at all Icylicious Stores.
+              </p>
+              <div class="div-price-container">
+                <h3 class="color-dark-grey sans-bold">₱55.00</h3>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-12 div-product-info">
+              <img src="./assets/images/summer_promo_5_5_4.png" class="img-product" />
+              <div style="height: 20px;"></div>
+              <h4 class="color-dark-grey size-13 sans-700">
+                BUY 1 TAKE 1 REGULAR CHOCO CHIP SMOOTHIE PEARLS
+              </h4>
+              <p class="color-light-grey size-10 sans-regular">
+                Experience the ultimate taste of summer with our 5.5 Summer Promo!
+                Available at all Icylicious Stores.
+              </p>
+              <div class="div-price-container">
+                <h3 class="color-dark-grey sans-bold">₱155.00</h3>
+                <h5 class="strike-price color-super-light-grey sans-regular">₱170.00</h5>
+              </div>
+              <!-- <button class="btn btn-md btn-outline-primary sans-600 btn-add-to-cart" type="button" style="width: 100%;">
+                <i class="fa-solid fa-cart-plus"></i>&nbsp;&nbsp;Add to cart
+              </button> -->
+            </div>
+          </div>
+          <button class="btn btn-lg btn-primary sans-600" type="button" style="display: block; margin: auto; margin-top: 30px;">
+            <i class="fa-solid fa-gifts"></i>&nbsp;&nbsp;View More Products
+          </button>
+        </div>
+        <div id="reviews" style="margin-top: 70px;">
+          
+        </div>
+      </div>
     </div>
   </body>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
