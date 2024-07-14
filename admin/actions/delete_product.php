d74724e637bf8319ec4b7a33f6fc1f0d66313792
Norman Palisoc <palisocnorman@gmail.com>
2024-07-15 00:19:16 +0800

    feat: delete products

--- admin/actions/delete_product.php
+++ admin/actions/delete_product.php
@@ -10,11 +10,13 @@
         $delete_product_img_query = "DELETE FROM products_images WHERE product_id = ".$product_id."";
         $delete_product_price_query = "DELETE FROM products_prices WHERE product_id = ".$product_id."";
         $delete_promotions_query = "DELETE FROM promotions WHERE product_id = ".$product_id."";
+        $delete_best_sellers_query = "DELETE FROM best_sellers WHERE product_id = ".$product_id."";
         $conn->query($delete_product_info_query);
         $conn->query($delete_product_cat_query);
         $conn->query($delete_product_img_query);
         $conn->query($delete_product_price_query);
         $conn->query($delete_promotions_query);
+        $conn->query($delete_best_sellers_query);
         header('Location: ../products/');
       }
     }
