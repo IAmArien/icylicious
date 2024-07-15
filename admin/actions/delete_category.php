96d8e890a60dc942c41a62ef7bd145a259cd567b
Norman Palisoc <palisocnorman@gmail.com>
2024-07-15 20:24:30 +0800

    feat: delete products categories

--- admin/actions/delete_category.php
+++ admin/actions/delete_category.php
@@ -6,7 +6,9 @@
       if (isset($_POST['category_id'])) {
         $category_id = $_POST['category_id'];
         $delete_query = "DELETE FROM categories WHERE id = ".$category_id."";
+        $delete_product_category_query = "DELETE FROM products_categories WHERE category_id = ".$category_id."";
         $conn->query($delete_query);
+        $conn->query($delete_product_category_query);
         header('Location: ../categories/');
       }
     }
